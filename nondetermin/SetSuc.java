401985412ae74215cb3b1b508d6c9088a8ffc797
jacksonsix <jacksonsixmailbox@gmail.com>
2019-09-25 15:24:28 -0400

    Create SetSuc.java
    9-25

---
+++ nondetermin/SetSuc.java
@@ -0,0 +1,11 @@
+package nondetermin;
+
+public class SetSuc implements ISucObject{
+
+	@Override
+	public Object success(Object val, IFailObject fail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
